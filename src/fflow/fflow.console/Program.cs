7eaaa9163db775855baa095cbec39b44987d9102
Ralf Westphal <info@ralfw.de>
2015-05-16 09:42:35 +0200

    started project; console head; sample workflow

---
+++ src/fflow/fflow.console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CLAP;
+
+namespace fflow.console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var head = new Head ();
+			Parser.Run<Head> (args, head);
+		}
+	}
+
+
+
+	public class Head {
+		[Verb]
+		public void Open(
+			[Aliases("p,path,w,wf,workflow")] string workflowpath) {
+			Console.WriteLine ("Opened workflow: {0}", workflowpath);
+		}
+	}
+}
---
+++ src/fflow/fflow.console/run_3stations.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+rm -rf 3stations
+mkdir 3stations
+cp -r ../samples/3stations/* 3stations
+/usr/bin/mono fflow.console.exe open -w=3stations
